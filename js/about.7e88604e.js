cf69f742b00375836d1121eb9734d7239a49de89
Orion <omelgarejo@vikua.com>
2023-05-15 21:40:32 -0400

    deploy

---
+++ js/about.7e88604e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkscramel"]=self["webpackChunkscramel"]||[]).push([[443],{6922:function(n,s,t){t.r(s),t.d(s,{default:function(){return c}});var u=function(){var n=this;n._self._c;return n._m(0)},e=[function(){var n=this,s=n._self._c;return s("div",{staticClass:"about"},[s("h1",[n._v("This is an about page")])])}],a=t(1001),l={},r=(0,a.Z)(l,u,e,!1,null,null,null),c=r.exports}}]);
+//# sourceMappingURL=about.7e88604e.js.map
